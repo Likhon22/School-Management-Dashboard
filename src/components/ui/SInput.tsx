465581ae8eefceaed62cc6bcdf010f0103400a7e
Likhon22 <likhonsarker793@gmail.com>
2025-05-31 01:57:39 +0600

    initial commit

---
+++ src/components/ui/SInput.tsx
@@ -0,0 +1,22 @@
+import { InputHTMLAttributes } from "react";
+import clsx from "clsx";
+type InputProps = {
+  placeholder?: string;
+  type?: "text" | "password" | "email" | "number";
+  className?: string;
+} & InputHTMLAttributes<HTMLInputElement>;
+const SInput = ({ placeholder, type, className, ...props }: InputProps) => {
+  return (
+    <input
+      className={clsx(
+        "border rounded-md p-1 bg-white focus:outline-none focus:border-red-100 focus:ring-1 focus:ring-red-100 text-sm w-full",
+        className
+      )}
+      {...props}
+      placeholder={placeholder}
+      type={type}
+    />
+  );
+};
+
+export default SInput;
